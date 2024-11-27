6fa14e5c6a72fb797caf2e9d602823bde769efaa
BramadyaFiqri <135415389+bramadyafiqri@users.noreply.github.com>
2024-11-27 18:30:42 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,33 @@
+const texts = [
+    "3 Tahun Pengalaman!!",
+    "Saya seorang Developer Teknologi dan Penggemar Pembelajaran Mesin",
+    "Saya mengkhususkan diri dalam Pengembangan Web, AI, dan Pengembangan Game",
+    "3 Years experience!!",
+    "I'm a Tech Developer and Machine Learning Enthusiast",
+    "I specialize in Web Development, AI, and Game Development"
+];
+
+let currentTextIndex = 0;
+let currentCharIndex = 0;
+const textElement = document.getElementById("dynamicText");
+
+let typingInterval;
+let pauseDuration = 2500;
+
+function typeText() {
+    const textToType = texts[currentTextIndex];
+    textElement.textContent = textToType.slice(0, currentCharIndex);
+    currentCharIndex++;
+
+    if (currentCharIndex > textToType.length) {
+        clearInterval(typingInterval);
+
+        setTimeout(() => {
+            currentCharIndex = 0;
+            currentTextIndex = (currentTextIndex + 1) % texts.length;
+            typingInterval = setInterval(typeText, 100);
+        }, pauseDuration);
+    }
+}
+
+typingInterval = setInterval(typeText, 100);
